70aefc7bd67c500825d166e9be23d1d725dde7d2
IIIJonasIII <jonasolsson86@gmail.com>
2025-08-20 10:26:48 +0200

    albums

--- BandApp.Web/Models/Band.cs
+++ BandApp.Web/Models/Band.cs
@@ -2,9 +2,21 @@
 
 public class Band
 {
+    //public Band(int id, string name, string? description, string? picture, string? video, List<string>? album)
+    //{
+    //    Id = id;
+    //    Name = name;
+    //    Description = description;
+    //    Picture = picture;
+    //    Video = video;
+    //    Album = album;
+    //}
+
     public int Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
     public string? Picture { get; set; }
+    public string? Video { get; set; }
+    public List<string>? Album { get; set; }
 
 }
